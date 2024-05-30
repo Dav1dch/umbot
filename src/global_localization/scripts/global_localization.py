a3b45924ba1587b2e78ca24f7b835eb49f225019
David <dav1dch@outlook.com>
2024-05-30 13:24:31 +0800

    rospy hello world

--- src/global_localization/scripts/global_localization.py
+++ src/global_localization/scripts/global_localization.py
@@ -1,2 +1,20 @@
 #! /usr/bin/env python3
 # coding:utf-8
+#
+
+import rospy
+import numpy as np
+import sys
+
+print(sys.path)
+
+
+def main():
+    rospy.init_node('rospyhello')
+    print("hello world from ros py")
+
+if __name__ == "__main__":
+    main()
+
+
+
