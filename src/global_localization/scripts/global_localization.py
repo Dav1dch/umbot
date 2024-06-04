528f69eae814268b582e11ab0ae4e19646f6c655
David <dav1dch@outlook.com>
2024-06-05 00:51:46 +0800

    python subscribe to image

--- src/global_localization/scripts/global_localization.py
+++ src/global_localization/scripts/global_localization.py
@@ -3,16 +3,27 @@
 #
 
 import rospy
+import sensor_msgs
+from sensor_msgs.msg import Image
 import numpy as np
 import sys
 import torch
 
 print(sys.path)
 
+def callback_image(data):
+    rospy.loginfo("data from image topic")
+    print(data.data)
+
 
 def main():
-    rospy.init_node('rospyhello')
+    rospy.init_node('global-localization')
     print("hello world from ros py")
+    rospy.Subscriber("/camera/color/image_raw", Image, callback_image)
+
+    rospy.spin()
+
+
 
 if __name__ == "__main__":
     main()
