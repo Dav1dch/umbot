7fde2a4abf55e37c3866dbe3a57842b26f489d3e
David <dav1dch@outlook.com>
2024-06-07 01:25:41 +0800

    store embeddins and timestamp

--- src/FAST_LIO/scripts/construct_database.py
+++ src/FAST_LIO/scripts/construct_database.py
@@ -9,17 +9,29 @@ from sensor_msgs.msg import Image
 import torch
 import torchvision
 import numpy as np
+import os
+
+print(os.getcwd())
 
 model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights)
 model = model.eval()
+
+timestamps = []
+embeddings = []
 
 def callback_db(data):
     rospy.loginfo("receive image")
     im = np.frombuffer(data.data, dtype=np.uint8).reshape(data.height, data.width, -1)
     im = torch.from_numpy(im).type(dtype=torch.float)
     with torch.no_grad():
-        print(model(im.unsqueeze(0).view(1, 3, 640, 480)))
+        stamp = str(data.header.stamp.secs) + "." + str(data.header.stamp.nsecs)
+        timestamps.append(stamp)
+        # print(data.header)
+        embedding = model(im.unsqueeze(0).view(1,3 , 640, 480))
+        embeddings.append(embedding.numpy())
     # print(model(data.data))
+    np.save("/root/Code/umbot/src/FAST_LIO/scripts/timestamps.npy", timestamps)
+    np.save("/root/Code/umbot/src/FAST_LIO/scripts/embeddings.npy", embeddings)
 
 def main():
     rospy.init_node('construct-database')
